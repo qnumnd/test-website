5dbaa68cde1ce1fee3ca6c4efa739cb9f90b3022
py2048 <quannhn2001@gmail.com>
2024-03-11 18:09:46 +0700

    json fi

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-fetch('simdata.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Work with your JSON data here
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
-  });
+// fetch('simdata.json')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Work with your JSON data here
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with your fetch operation:', error);
+//   });
+data = fetch('simdata.json')
+console.log(data)
